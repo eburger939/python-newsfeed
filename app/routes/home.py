52a3381ff846b3f04b185690cfe7e77d10dd3a69
Emily Burger <eburger939@gmail.com>
2022-03-31 13:20:44 -0500

    first module with completed routes

---
+++ app/routes/home.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+#blueprint allows you to consolidate routes into a single object (bp) that the parent app can register later
+#similar to using router middelware when using express.js
+
+bp = Blueprint('home', __name__, url_prefix='/')
+
+@bp.route('/')
+#@bp.route before the function turns it into a route 
+def index():
+    return render_template('homepage.html')
+    #return is the response, using render_template allows to return a template instead of a string
+@bp.route('/login')
+def login():
+    return render_template('login.html')
+
+@bp.route('/post/<id>')
+#<id> is basically the same as a parameter in the url route 
+def single(id):
+    return render_template('single-post.html')
