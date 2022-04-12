8f94861aab0b8fc6fcab873768ed924552c926b1
Emily Burger <eburger939@gmail.com>
2022-04-12 18:49:18 -0500

    added votes, lesson 2 complete

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 from email.policy import default
-
 from pymysql import Date
 from app.db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from .Vote import Vote
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
+from sqlalchemy.orm import relationship, column_property 
 
 
 class Post(Base):
@@ -17,3 +17,7 @@ class Post(Base):
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
     user = relationship('User')
     comments = relationship('Comment', cascade='all,delete')
+    vote_count = column_property(
+        select([func.count(Vote.id)]).where(Vote.post_id ==id)
+    )
+    votes = relationship('Vote', cascade='all,delete')
